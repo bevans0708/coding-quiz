c676d6a5b2ddc5a74b6e575996d72934f2893a73
Brandon Evans <brandon.evans0708@gmail.com>
2021-06-02 13:48:03 -0600

    added styling to quiz container

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+var testQuestions = [ 
+   {
+      question: "",
+      answers: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "",
+      answers: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "",
+      answers: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "",
+      answers: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "",
+      answers: {
+        a: "",
+        b: "",
+        c: "",
+        d: ""
+      },
+      correctAnswer: "d"
+    }
+];
+
+var startQuizEl = document.getElementById("start-btn")
+
+
+function startQuiz() {
+   alert("It Worked")
+};
+
+startQuizEl.addEventListener("click", startQuiz);
